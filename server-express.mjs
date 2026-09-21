19d811b336d3781ecf4a16fdcd6beb9f55e23cb9
Bill_Mx <mazebillye119@gmail.com>
2026-09-21 13:57:21 +1100

    Partie 2 : corresction midelwares

--- server-express.mjs
+++ server-express.mjs
@@ -1,30 +1,50 @@
 import express from "express";
 import morgan from "morgan";
+import createError from "http-errors";
+import logger from "loglevel";
+
+logger.setLevel(logger.levels.DEBUG);
 
 const host = "localhost";
 const port = 8000;
 
 const app = express();
 
-app.get(["/", "/index.html"], async function (request, response, next) {
-  response.sendFile("index.html", { root: "./" });
+if (app.get("env") === "development") app.use(morgan("dev"));
+
+app.use(express.static("static"));
+
+app.set("view engine", "ejs");
+
+app.get("/random/:nb", async function (request, response, next) {
+    const length = Number.parseInt(request.params.nb, 10);
+    if (Number.isNaN(length)) {
+        return next(createError(400));
+     }
+  const numbers = Array.from({ length }).map(() => Math.floor(100 * Math.random()));
+  const welcome = "Bienvenue sur la page random !";
+  return response.render("random", { numbers, welcome });
 });
 
-app.get(response.render("random", {numbers, welcome}), async function (request, response, next) {
-  const length = request.params.nb;
-  const contents = Array.from({ length })
-    .map((_) => `<li>${Math.floor(100 * Math.random())}</li>`)
-    .join("\n");
-  return response.send(`<html><ul>${contents}</ul></html>`);
+app.use((request, response, next) => {
+    logger.debug(`default route handler : ${request.url}`);
+    return next(createError(404));
+});
+
+app.use((error, _request, response, _next) => {
+    logger.debug(`default error handler: ${error}`);
+    const status = error.status ?? 500;
+    const stack = app.get("env") === "development" ? error.stack : "";
+    const result = { code: status, message: error.message, stack };
+    return response.render("error", result);
 });
 
 const server = app.listen(port, host);
 
 server.on("listening", () =>
-  console.info(
-    `HTTP listening on http://${server.address().address}:${server.address().port} with mode '${process.env.NODE_ENV}'`,
+    logger.info(
+        `HTTP listening on http://${server.address().address}:${server.address().port} with mode '${process.env.NODE_ENV}'`,
   ),
 );
 
-console.info(`File ${import.meta.url} executed.`);
-pp.set("view engine", "ejs");
+logger.info(`File ${import.meta.url} executed.`);
